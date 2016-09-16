751610430b329503a097c71050dc2816188bf1e8
Andrew Tae <andrewtae3368@gmail.com>
2016-09-15 22:28:19 -0700

    implement queue and map

--- w1d4/rbdiyadt.rb
+++ w1d4/rbdiyadt.rb
@@ -12,7 +12,66 @@ class Stack
   end
 
   def show
-    @stack
+    puts @stack
   end
 
+end
+
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue.unshift(el)
+  end
+
+  def dequeue
+    @queue.pop
+  end
+
+  def show
+    puts @queue
+  end
+end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key,value)
+    @map = @map.each do |pairs|
+      if pairs[0] == key
+        pairs[1] = value
+        return @map
+      end
+
+    end
+
+    @map += [[key,value]]
+  end
+
+  def lookup(key)
+    @map.each do |pairs|
+      return pairs[1] if pairs[0] == key
+    end
+    return nil
+  end
+
+  def remove(key)
+    removal_idx = nil
+    @map.each_with_index do |pairs, index|
+      if pairs[0] == key
+        removal_idx = index
+      end
+    end
+    return nil if removal_idx == nil
+    @map.delete_at(removal_idx)
+
+  end
+
+  def show
+    @map
+  end
 end
